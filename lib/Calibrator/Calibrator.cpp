f28f2d5381984b8ed4dcc9242d514f8e70a88398
m0nkey653 <m0nkey653@gmail.com>
2026-09-10 19:00:24 -0400

    added callibration and print

--- lib/Calibrator/Calibrator.cpp
+++ lib/Calibrator/Calibrator.cpp
@@ -1,14 +1,30 @@
 #include "Calibrator.h"
 
+const float unitsPerGram = 373.576f;
+const float unitsPerMilligram = .373576f;
+
 void Calibrator::ZeroCalibratedReading(DualChannelReadings readings)
 {
     _zeroOffsetChannel0 = readings.channel0;
     _zeroOffsetChannel1 = readings.channel1;
+    Serial.printf("Zero calibration complete. Offsets: CH0=%d, CH1=%d\n", _zeroOffsetChannel0, _zeroOffsetChannel1);
 }
+
+/*
+500g = 186788
+1g = 373.576
+1mg = 0.367576
+*/
 
 uint32_t Calibrator::Calculate(DualChannelReadings readings)
 {
     readings.channel0 -= _zeroOffsetChannel0;
     readings.channel1 -= _zeroOffsetChannel1;
-    return readings.channel0 + readings.channel1;
+    Serial.printf("Calibrated readings: CH0=%d, CH1=%d\n", readings.channel0, readings.channel1);
+    int32_t readingSum = readings.channel0 + readings.channel1;
+    Serial.printf("Sum of calibrated readings: %d\n", readingSum);
+    Serial.printf("Units per gram: %.6f\n", unitsPerGram);
+    readingSum = max(readingSum, (int32_t)(0));
+    Serial.printf("Sum of calibrated readings (zeroed): %d\n", readingSum);
+    return static_cast<uint32_t>(readingSum / unitsPerGram);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
 #include "Nau7802.h"
+#include "Calibrator.h"
 
 const int BUTTON_PIN = 9;
 volatile bool buttonFlag = false;
 
 NauAdc _adc;
+Calibrator _calibrator;
+uint64_t _startMills = 0;
+uint32_t _intervalLength = 1000; // 1 second
+// unsigned long _elapsedTime = 0;
 
 void IRAM_ATTR handleButtonPress()
 {
@@ -15,20 +20,39 @@ void setup()
     Serial.begin(115200);
     delay(500);
     Serial.println("Starting PearlHq");
-    _adc.initialize();
 
     // Set up pin with internal pull-up resistor
     pinMode(BUTTON_PIN, INPUT_PULLUP);
     attachInterrupt(digitalPinToInterrupt(9), handleButtonPress, FALLING);
+
+    _adc.initialize();
+    DualChannelReadings readings = _adc.getReadings();
+    _calibrator.ZeroCalibratedReading(readings);
+    Serial.printf("Initial readings: CH0=%d, CH1=%d\n", readings.channel0, readings.channel1);
+    Serial.println("Calibration complete. Press the button to get calibrated readings.");
+
+    _startMills = millis();
 }
 
 void loop()
 {
-    // Serial.printf("Button state: %s\n", buttonFlag ? "Pressed" : "Not Pressed");
+    if ((millis() - _startMills) >= _intervalLength)
+    {
+        _startMills = millis();
+
+        DualChannelReadings readings = _adc.getReadings();
+        Serial.printf("Raw readings: CH0=%d, CH1=%d\n", readings.channel0, readings.channel1);
+        uint32_t calculatedMg = _calibrator.Calculate(readings);
+        Serial.printf("----------------------\n");
+        Serial.printf("Value: %d g\n", calculatedMg);
+        Serial.printf("----------------------\n");
+        buttonFlag = false; // Reset the flag after handling the button press
+        delay(1000);        // Delay to avoid flooding the serial output
+    }
+    // Serial.printf("Waiting for button press... %d\n", _startMills);
+
     if (buttonFlag)
     {
-        DualChannelReadings readings = _adc.getReadings();
-        Serial.printf("Button pressed! Readings: CH0=%d, CH1=%d\n", readings.channel0, readings.channel1);
-        buttonFlag = false; // Reset the flag after handling the button press
+        Serial.println("Button pressed! Getting calibrated readings...");
     }
 }
