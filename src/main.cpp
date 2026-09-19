fb5e7ace7fb113cc22347fe097301d7d36c1e01a
m0nkey653 <m0nkey653@gmail.com>
2026-09-19 13:04:07 -0400

    bundled in wifi code to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include "Nau7802.h"
 #include "Calibrator.h"
 
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include "secrets.h"
+
 const int BUTTON_PIN = 9;
 volatile bool buttonFlag = false;
 
@@ -20,6 +24,39 @@ void setup()
     Serial.begin(115200);
     delay(500);
     Serial.println("Starting PearlHq");
+
+    wl_status_t status = WiFi.begin(Secrets::SSID, Secrets::PASSWORD);
+    while (status != WL_CONNECTED)
+    {
+        switch (status)
+        {
+        case WL_NO_SSID_AVAIL:
+            Serial.println("SSID not available");
+            break;
+        case WL_CONNECT_FAILED:
+            Serial.println("Connection failed");
+            break;
+        case WL_IDLE_STATUS:
+            Serial.println("WiFi idle");
+            break;
+        case WL_DISCONNECTED:
+            Serial.println("WiFi disconnected");
+            break;
+        case WL_SCAN_COMPLETED:
+            Serial.println("WiFi scan completed");
+            break;
+        default:
+            Serial.println("Connecting...");
+            break;
+        }
+        delay(500);
+        Serial.print(".");
+        status = WiFi.status();
+    }
+    Serial.println("\nConnected to WiFi!");
+    WiFi.setAutoReconnect(true);
+    auto ip = WiFi.broadcastIP();
+    Serial.printf("Broadcast IP: %s\n", ip.toString().c_str());
 
     // Set up pin with internal pull-up resistor
     pinMode(BUTTON_PIN, INPUT_PULLUP);
